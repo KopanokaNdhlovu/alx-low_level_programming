1d7dd1a5e643dbdf839f7558bd5717126a3863c8
KopanokaNdhlovu <kopanokandhlovu@gmail.com>
2022-09-15 07:28:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,22 @@
-#include <stdio.h>                                                                                                          
-                                                                                                                            
-/**                                                                                                                         
- *main - print the string in the put function                                                    
- *
- *Description: using the main function                                                                                    
- *this program prints "Programming is like building a multilingual puzzle                                                 
- * *Return: 0                                                                                                                */                                                                                                                       
-int main(void)                                                                                                              
+#include <stdio.h>
 
-{                                                                                                                           
-	  printf("with the proper grammer, but the outcome is a piece of art");                                                       return (0);                                                                                                          
-}                                                             
+
+
+/**
+ *
+ *  * main - print the string in the put function
+ *
+ *Description: using the main function
+ *this program prints "Programming is like building a multilingual puzzle
+ *    * Return: Always 0 
+ *
+ *     */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,")  
+      	return (0);
+
+}
